c6317746153eef6ecc2791a443ddebac9daab1e6
magnus.kodehode <magnus.kodehode@gmail.com>
2023-01-11 12:41:26 +0100

    Completed project

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Countdown from './Countdown'
+function Nav() {
+    return (
+        <nav className='Nav'>
+            <Countdown />
+        </nav>
+    )
+}
+
+export default Nav
